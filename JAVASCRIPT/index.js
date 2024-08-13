48f16056fbbe318c72e3b5750911ecb514b34c51
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2024-08-14 01:38:32 +0700

    Delete JAVASCRIPT directory

--- JAVASCRIPT/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World")
-console.log("lorem ipsum")
-
-//window.alert("This is an Alert")
-
-document.getElementById("myH1").textContent = "Haiiii" ;
-document.getElementById("myP").textContent = "I Love Liaaaaaaaa" ;
