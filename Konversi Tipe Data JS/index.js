87a23bb57afd874c21c2e0e77aca53c389a3cee4
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2024-08-15 21:58:16 +0700

    Add files via upload

---
+++ Konversi Tipe Data JS/index.js
@@ -0,0 +1,17 @@
+/*let saldo = window.prompt("berapa sisa saldo anda?");
+
+saldo = Number(saldo);
+saldo += 2;
+
+console.log(saldo, typeof saldo);
+*/
+
+let variabel = 0;
+
+Number = Number(variabel);
+String = String(variabel);
+Boolean = Boolean(variabel);
+
+console.log(Number, typeof Number);
+console.log(String, typeof String);
+console.log(Boolean, typeof Boolean);
