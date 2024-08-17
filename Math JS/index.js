60f71822e2f669c5d1b36909cc8a5f3ad56aeb20
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2024-08-17 21:02:47 +0700

    Add files via upload

---
+++ Math JS/index.js
@@ -0,0 +1,24 @@
+// math = objek yang berada di javascript yang memiliki properti dan metode dalam matematika
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 45;
+let y = 2;
+let z = 2;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+//z = Math.sqrt(x, y);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+//z = Math.sign(x);
+let max = Math.max(x, y, z)
+console.log(z);
+console.log(max)
