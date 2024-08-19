b6866e65ef2649fe1be940dade596a20e3b724d2
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2024-08-19 07:52:45 -0700

    Add files via upload

---
+++ String Methods JS/index.js
@@ -0,0 +1,34 @@
+// string methods : allow you to work and manipulate a string
+
+let userName  = "Haqi";
+
+//console.log(userName.charAt(3));
+//console.log(userName.indexOf("i"));
+//console.log(userName.lastIndexOf("q"));
+//console.log(userName.length);
+//userName = userName.trim();
+//console.log(userName);
+//console.log(userName.toUpperCase());
+//console.log(userName.repeat(6));
+//console.log(userName.endsWith("H"));
+//console.log(userName.includes("q"));
+let phoneNum = "891-312-312";
+//console.log(phoneNum.replaceAll("-", ""))
+//console.log(phoneNum.padStart(30, "2"))
+console.log(phoneNum.padEnd(16, "="))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
